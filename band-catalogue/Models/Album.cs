d87abacb143498779a3a7e6c514ca8f1c0c77dec
Jeremiah Pritchard <jeremiah3643@gmail.com>
2025-03-06 11:02:46 -0600

    creation of initial models

---
+++ band-catalogue/Models/Album.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace band_catalogue.Models
+{
+    public class Album
+    {
+        [Key]
+        public int AlbumId { get; set; }
+
+        [Required]
+        [Display(Name = "Album Title")]
+        public string Title { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+        // Foreign Key linking the Album to a Band
+        [ForeignKey("Band")]
+        public int BandId { get; set; }
+        public Band Band { get; set; }
+
+        // One-to-Many: An Album can have multiple Songs
+        public List<Song> Songs { get; set; } = new List<Song>();
+    }
+}
---
+++ band-catalogue/Models/Song.cs
@@ -0,0 +1,23 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace band_catalogue.Models
+{
+    public class Song
+    {
+        [Key]
+        public int SongId { get; set; }
+
+        [Required]
+        [Display(Name = "Song Title")]
+        public string Title { get; set; }
+
+        public TimeSpan Duration { get; set; }
+
+        // Foreign Key linking this song to an Album
+        [ForeignKey("Album")]
+        public int AlbumId { get; set; }
+        public Album Album { get; set; }
+    }
+}
